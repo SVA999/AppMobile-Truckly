a417d7403f0d68c03b2812b79047cc6a0607fd5a
SVA999 <hhh.sva999@gmail.com>
2026-08-22 16:03:06 -0500

    FleetContext contract

---
+++ trucklyApp/src/data/FleetContext.ts
@@ -0,0 +1,13 @@
+import { createContext } from 'react'
+import type { DocumentKind, Truck, TruckEditableFields } from './types'
+
+export interface FleetContextValue {
+  trucks: Truck[]
+  getTruck: (id: string) => Truck | undefined
+  addTruck: (truck: Omit<Truck, 'id'>) => Truck
+  updateTruck: (truckId: string, patch: Partial<TruckEditableFields>) => void
+  updateDocument: (truckId: string, kind: DocumentKind, expiresOn: string) => void
+  clearAllData: () => void
+}
+
+export const FleetContext = createContext<FleetContextValue | null>(null)
